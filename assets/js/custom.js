7891eb0179152cdbe27f4d051dfd947f9dea5015
MIV <roska.posti@feellike.fi>
2020-01-03 18:10:50 +0200

    search started

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,32 @@
 window.onload = function() {
+    //Show componets that require javascript
+    var allowed = document.querySelectorAll('.js-allowed');
+    var denied = document.querySelectorAll('.js-deny');
+
+    for (var i = 0; i < denied.length; i++) {
+        denied[i].style.display = 'none';
+    }
+    for (var i = 0; i < allowed.length; i++) {
+        allowed[i].style.display = 'block';
+    }
+
+
+    //Show deferred images
     var images = document.querySelectorAll('.lazy-img');
     for (var i = 0; i < images.length; i++) {
         images[i].setAttribute('src',images[i].getAttribute('data-src'));
     }
-}
+};
+
+changeFocus = function() {
+    var result = document.getElementById('search-result');
+    var input = document.getElementById('search');
+
+    if(result.classList.contains("focused")) {
+        result.classList
+    }
+    console.log(result);
+    console.log(input);
+
+};
+//
