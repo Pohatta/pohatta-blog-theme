4d06f4a7d89ebed375fe6de79d162e97d4204dd4
MIV <roska.posti@feellike.fi>
2020-03-12 21:39:13 +0200

    changed post end text

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,40 @@
 window.onload = function() {
     //Show componets that use javascript
-    // var allowed = document.querySelectorAll('.js-allowed');
-    // var denied = document.querySelectorAll('.js-deny');
+    var allowed = document.querySelectorAll('.js-allowed');
+    var denied = document.querySelectorAll('.js-deny');
 
-    // for (var i = 0; i < denied.length; i++) {
-    //     denied[i].style.display = 'none';
-    // }
-    // for (var i = 0; i < allowed.length; i++) {
-    //     allowed[i].style.display = 'block';
-    // }
+    for (var i = 0; i < denied.length; i++) {
+        denied[i].style.display = 'none';
+    }
+    for (var i = 0; i < allowed.length; i++) {
+        allowed[i].style.display = 'block';
+    }
 
 
-    // //Show deferred images
-    // var images = document.querySelectorAll('.lazy-img');
-    // for (var i = 0; i < images.length; i++) {
-    //     images[i].setAttribute('src',images[i].getAttribute('data-src'));
-    // }
+    //Show deferred images
+    var images = document.querySelectorAll('.lazy-img');
+    for (var i = 0; i < images.length; i++) {
+        images[i].setAttribute('src',images[i].getAttribute('data-src'));
+    }
 
-    // //Show post-card-images
-    // var postCardImages = document.querySelectorAll('.post-card-image');
-    // for (var i = 0; i < postCardImages.length; i++) {
-    //     postCardImages[i].style.display = 'block';
-    // }
+    //Show post-card-images
+    var postCardImages = document.querySelectorAll('.post-card-image');
+    for (var i = 0; i < postCardImages.length; i++) {
+        postCardImages[i].style.display = 'block';
+    }
 
-    // //Hide social menu nav in ie, breaks the site otherwise 
-    // //Detect if ie: https://stackoverflow.com/questions/19999388/check-if-user-is-using-ie
-    // var ua = window.navigator.userAgent;
-    // var msie = ua.indexOf("MSIE ");
+    //Hide social menu nav in ie, breaks the site otherwise 
+    //Detect if ie: https://stackoverflow.com/questions/19999388/check-if-user-is-using-ie
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
 
-    // if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-    // {
-    //     var rightNav = document.querySelectorAll('.site-nav-right');
-    //     if(rightNav) {
-    //         rightNav[0].style.display = 'none';
-    //     }
-    // }
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        var rightNav = document.querySelectorAll('.site-nav-right');
+        if(rightNav) {
+            rightNav[0].style.display = 'none';
+        }
+    }
 
 };
 
